b190a86b522d7076435d985564cfabbf0f8ece29
sreenieadara <sreeni.eadara@gmail.com>
2023-11-29 00:53:59 -0600

    USB datalogging and serial printing of data work. Unclear why recording lasts for ~200K ms.

--- RTD-thermometer.ino
+++ RTD-thermometer.ino
@@ -3,6 +3,10 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 #include <Adafruit_MAX31865.h>
+#include <string.h>
+
+#include "SdFat.h"
+#include "msc_data_logger/usbh_helper.h"
 
 Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
 
@@ -15,8 +19,15 @@ Adafruit_MAX31865 thermo = Adafruit_MAX31865(4); // SREENI hardware SPI0 (25, 18
 #define RREF      430.0
 #define RNOMINAL  100.0
 
+Adafruit_USBH_MSC_BlockDevice msc_block_dev;
+
+FatVolume fatfs;
+File32 f_log;
+
 void setup() {
   Serial.begin(115200);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
   delay(250); // wait for the OLED to power up
   display.begin(0x3C, true); // Address 0x3C default
@@ -57,6 +68,7 @@ void setup() {
 
 volatile int state = 0;
 volatile long elapsed = 0;
+volatile bool is_mounted = false;
 
 const int LOOP_DURATION = 10;
 const long SLEEP_DURATION = 10000;
@@ -121,9 +133,12 @@ void loop() {
     display.println("RECORDING STOPPED.");
     display.println("");
     display.println("");
+    f_log.close();
   }
   else if(state == 2){
     float temp = getTemp();
+    writeTemp(temp);
+    Serial.printf("%i\t%f\n", millis(), temp);
     display.println("STARTED RECORDING.");
     display.print("TEMP: ");
     display.println(temp);
@@ -144,11 +159,106 @@ void loop() {
   
 }
 
-void startUSB() {
-  delay(10);
-}
-
 float getTemp() {
   uint16_t rtd = thermo.readRTD();
   return thermo.temperature(RNOMINAL, RREF);
 }
+
+void writeTemp(float temp) {
+  if (!is_mounted) {
+    return;
+  }
+  String csvName = "TEMPERATURE_DATA.tsv";
+  /*String newcsvName = "ERR.csv";
+  for (int i = 1; i <= 256; i++) {
+    newcsvName = i + csvName;
+    if (!fatfs.exists(newcsvName)) {
+      break;
+    }
+  }*/
+  digitalWrite(LED_BUILTIN, HIGH);
+  if (!f_log) {
+    f_log = fatfs.open(csvName, O_WRITE | O_APPEND | O_CREAT);
+  } else {
+    f_log.printf("%i\t%f\n", millis(), temp);
+  }
+  
+  //Serial.flush();
+}
+
+
+
+//------------- Core1 -------------//
+void setup1() {
+  // configure pio-usb: defined in usbh_helper.h
+  rp2040_configure_pio_usb();
+
+  // run host stack on controller (rhport) 1
+  // Note: For rp2040 pico-pio-usb, calling USBHost.begin() on core1 will have most of the
+  // host bit-banging processing works done in core1 to free up core0 for other works
+  USBHost.begin(1);
+}
+
+void loop1() {
+  USBHost.task();
+}
+
+//--------------------------------------------------------------------+
+// TinyUSB Host callbacks
+//--------------------------------------------------------------------+
+bool write_complete_callback(uint8_t dev_addr, tuh_msc_complete_data_t const *cb_data) {
+  (void) dev_addr;
+  (void) cb_data;
+
+  // turn off LED after write is complete
+  // Note this only marks the usb transfer is complete, device can take longer to actual
+  // write data to physical flash
+  digitalWrite(LED_BUILTIN, LOW);
+
+  return true;
+}
+
+extern "C"
+{
+
+// Invoked when device is mounted (configured)
+void tuh_mount_cb(uint8_t daddr) {
+  (void) daddr;
+}
+
+/// Invoked when device is unmounted (bus reset/unplugged)
+void tuh_umount_cb(uint8_t daddr) {
+  (void) daddr;
+}
+
+// Invoked when a device with MassStorage interface is mounted
+void tuh_msc_mount_cb(uint8_t dev_addr) {
+  // Initialize block device with MSC device address
+  msc_block_dev.begin(dev_addr);
+
+  // For simplicity this example only support LUN 0
+  msc_block_dev.setActiveLUN(0);
+  msc_block_dev.setWriteCompleteCallback(write_complete_callback);
+  is_mounted = fatfs.begin(&msc_block_dev);
+
+  if (is_mounted) {
+    fatfs.ls(&Serial, LS_SIZE);
+  } else {
+    Serial.println("Failed to mount mass storage device. Make sure it is formatted as FAT");
+  }
+}
+
+// Invoked when a device with MassStorage interface is unmounted
+void tuh_msc_umount_cb(uint8_t dev_addr) {
+  (void) dev_addr;
+
+  // unmount file system
+  is_mounted = false;
+  fatfs.end();
+
+  // end block device
+  msc_block_dev.end();
+}
+
+}
+
